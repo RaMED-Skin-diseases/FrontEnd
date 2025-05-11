3883a47c94c402e53b39b3ff12ccf335064a6c3b
KareemAbdelnabi <kareem.mahmoud02@eng-st.cu.edu.eg>
2025-05-11 12:54:40 +0300

    final hometabs

--- HomeTabs.js
+++ HomeTabs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialIcons, Ionicons } from '@expo/vector-icons'; // Import icons
 import HomeScreen from './screens/HomeScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import SettingsScreen from './screens/SettingsScreen';
@@ -7,13 +8,50 @@ import CommunityScreen from './screens/CommunityScreen';
 
 const Tab = createBottomTabNavigator();
 
-export default function HomeTabs() {
+export default function HomeTabs({ route }) {
+  const identifier = route.params?.identifier || '';
+
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+  screenOptions={({ route }) => ({
+    tabBarIcon: ({ color, size }) => {
+      let iconName;
+
+      if (route.name === 'Home') {
+        iconName = 'home';
+      } else if (route.name === 'Profile') {
+        iconName = 'person';
+      } else if (route.name === 'Settings') {
+        iconName = 'settings';
+      } else if (route.name === 'Community') {
+        iconName = 'people';
+      }
+
+      return <MaterialIcons name={iconName} size={22} color={color} />;
+    },
+    tabBarActiveTintColor: '#A0C4FF',
+    tabBarInactiveTintColor: 'gray',
+    tabBarStyle: {
+      backgroundColor: '#FFFFFF',
+      height: 60,               
+      paddingBottom: 4,        
+      paddingTop: 0,
+    },
+    tabBarLabelStyle: {
+      fontSize: 11,
+      marginBottom: 12,          
+    },
+  })}
+>
+
       <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
       <Tab.Screen name="Community" component={CommunityScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen 
+        name="Settings" 
+        component={SettingsScreen} 
+        initialParams={{ identifier }}
+      />
     </Tab.Navigator>
   );
 }
